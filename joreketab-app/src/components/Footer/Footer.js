7ed273a99b4ea8f43a6a0aba91ab67a1acc07279
atefe-fazeli <atefefazeli1996@gmail.com>
2022-12-09 14:43:11 +0330

    footer component created

---
+++ joreketab-app/src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+    return (
+        <div className={styles.footerMainContainer}>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+
+            
+        </div>
+    );
+};
+
+export default Footer;
